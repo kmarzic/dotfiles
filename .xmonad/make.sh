4b0372717337f32a4484ae596a71da296e88dea2
Kresimir Marzic <kresimir.marzic@ericsson.com>
2020-07-23 20:31:26 +0200

    .

--- .xmonad/make.sh
+++ .xmonad/make.sh
@@ -4,18 +4,20 @@ export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin"
 export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:/opt/ghc/bin"
 export PATH="$PATH:$HOME/.cabal/bin"
+# export PATH="$PATH:$HOME/.local/bin"
 PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++')
 
 ## Remove xmonad files
-cd ~/.xmonad
-rm -f xmonad.errors xmonad.hi xmonad.o xmonad-x86_64-linux xmonad.state xmonad
+cd ~/.xmonad && rm -f xmonad.errors xmonad.hi xmonad.o xmonad-x86_64-linux xmonad.state xmonad
 
 ## (1) - do not use sandbox
 # ghc --make xmonad.hs
-xmonad --recompile
 
 ## (2) - use sandbox
 # sandbox_dir=$(dirname ~/data/cabal/xmonad/cabal.sandbox.config)
 # cabal --require-sandbox --sandbox-config-file=${sandbox_dir}/cabal.sandbox.config v1-exec -- ghc --make xmonad.hs
+
+## (3) - stack
+# stack --stack-yaml stack.yaml ghc -- --make xmonad.hs -i -fforce-recomp -main-is main -v0
 
 ## END
--- .xmonad/recompile.sh
+++ .xmonad/recompile.sh
@@ -4,20 +4,25 @@ export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin"
 export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:/opt/ghc/bin"
 export PATH="$PATH:$HOME/.cabal/bin"
+# export PATH="$PATH:$HOME/.local/bin"
 PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++')
 
 ## (1) - do not use sandbox
-xmonad --recompile
-xmonad --restart
+# xmonad --recompile
+# xmonad --restart
 
 ## (2) - use sandbox
 # cd ~/data/cabal/xmonad
 # cabal v1-exec -- xmonad --recompile
 # cabal v1-exec -- xmonad --restart
 
-## (3) - use sandbox
+## (2) - use sandbox
 # sandbox_dir=$(dirname ~/data/cabal/xmonad/cabal.sandbox.config)
 # cabal --require-sandbox --sandbox-config-file=${sandbox_dir}/cabal.sandbox.config v1-exec -- xmonad --recompile
 # cabal --require-sandbox --sandbox-config-file=${sandbox_dir}/cabal.sandbox.config v1-exec -- xmonad --restart
+
+## (3) - stack
+~/bin/xmonad --recompile
+~/bin/xmonad --restart
 
 ## END
