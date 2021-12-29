7c6363aafd24ecbbd7a84bf6d5d52e57b5d592e4
Kresimir Marzic <kresimir.marzic@ericsson.com>
2021-12-29 15:05:40 +0100

    updatge

---
+++ bin/dusk.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+export PATH="${HOME}/bin:/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin"
+
+#### dusk status
+
+#### dusk battery
+
+#### trayer
+# trayer.sh &
+[[ -e /usr/bin/dropbox ]] && /usr/bin/dropbox start &
+[[ -e /usr/bin/nm-applet ]] && /usr/bin/nm-applet &
+
+#### xrandr
+[[ -e ${HOME}/bin/screen.toggle.sh ]] && screen.toggle.sh -x
+
+#### themes
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s ansi
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s base16-atelier-lakeside-light
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s base16-google-light
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s base16-gruvbox-dark-soft
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s dracula
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s everforest
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s gruvbox
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s monokai
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s nord
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s papercolor.light
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s solarized.dark
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s solarized.light
+# [[ -e ${HOME}/bin/screen.togggle.sh ]] && screen.togggle.sh -s srcery
+
+#### dusk
+while true;
+do
+    #### Log stderror to a file
+    ${HOME}/bin/dusk 2> ~/.dusk.log
+
+    #### No error logging
+    # ${HOME}/bin/dusk >/dev/null 2>&1
+done
+
+#### END
