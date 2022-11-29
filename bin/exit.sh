8f468d5076f5392c60bccaa535427cf989cdb420
Kresimir Marzic <kresimir.marzic@ericsson.com>
2022-11-29 13:30:13 +0100

    update

--- bin/exit.sh
+++ bin/exit.sh
@@ -21,7 +21,7 @@ function __lock()
     # i3lock -c 005088
 
     #### i3lock bluring
-    if [[ "$(hostname)" == "elx711804dr" ]] || [[ "$(hostname)" == "elxd648894f" ]]
+    if [[ "$(hostname)" == "elx711804dr" ]] || [[ "$(hostname)" == "elxd648894f" ]] || [[ "$(hostname)" == "entd0001" ]]
     then
         rm -f /var/tmp/screen_locked.png /var/tmp/screen_locked2.png
         scrot /var/tmp/screen_locked.png
