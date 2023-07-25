bec5f0d649cd2ec5d038b48d1422339269544867
Kresimir Marzic <kresimir.marzic@ericsson.com>
2023-07-25 06:10:36 +0200

    update

--- bin/dwm.sh
+++ bin/dwm.sh
@@ -36,13 +36,13 @@ export PATH="${HOME}/bin:/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin"
 # [[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s gruvbox.dark &
 # [[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s gruvbox.light &
 # [[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s monokai &
-[[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s nord &
+# [[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s nord &
 # [[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s papercolor.light &
 # [[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s selenized.dark &
 # [[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s selenized.light &
 # [[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s selenized.white &
 # [[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s solarized.dark &
-# [[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s solarized.light &
+[[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s solarized.light &
 # [[ -e ${HOME}/bin/dwm.status.sh ]] && dwm.status.sh -s srcery &
 
 #### dwm battery
