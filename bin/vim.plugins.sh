071e14cb0ec237c2872fd8d7223638676df08383
Kresimir Marzic <kresimir.marzic@ericsson.com>
2024-05-20 15:47:37 +0200

    update: nvim

--- bin/vim.plugins.sh
+++ bin/vim.plugins.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-export PATH=${HOME}/bin:/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin
+#### PATH
+export __PATH_ORIG="${PATH}"
+#
+export __PATH_NEW="${HOME}/bin:${HOME}/.local/bin:/usr/local/bin"
+export __PATH_NEW="${__PATH_NEW}:/opt/perl5/bin:/opt/perl5/cpan/bin:${HOME}/perl5/bin"
+# export __PATH_NEW="${__PATH_NEW}:/opt/cperl5/bin:/opt/cperl5/cpan/bin"
+# export __PATH_NEW="${__PATH_NEW}:/opt/rakudo-star-2017.10/bin:/opt/rakudo-star-2017.10/share/perl6/site/bin"
+export __PATH_NEW="${__PATH_NEW}:/opt/python3/bin:/opt/python3.ext/bin"
+export __PATH_NEW="${__PATH_NEW}:/opt/emacs/bin"
+export __PATH_NEW="${__PATH_NEW}:/opt/lua/bin:/opt/luarocks/bin"
+export __PATH_NEW="${__PATH_NEW}:/opt/tmux/bin"
+export __PATH_NEW="${__PATH_NEW}:/opt/rust/bin"
+export __PATH_NEW="${__PATH_NEW}:/opt/nvim/usr/bin"
+export __PATH_NEW="${__PATH_NEW}:${ORACLE_INSTANT_CLIENT}"
+export __PATH_NEW="${__PATH_NEW}:${GOROOT}/bin"
+export __PATH_NEW="${__PATH_NEW}:${GHCROOT}/bin"
+# export __PATH_NEW="${__PATH_NEW}:${HOME}/.cabal/bin"
+#
+export PATH="${__PATH_NEW}:${__PATH_ORIG}"
+# PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++')
 
 function __dirs_vim()
 {
@@ -58,6 +77,8 @@ function __dirs_nvim()
 
 function __neovim_bin()
 {
+    echo .
+
     #### nightly
     # echo "# curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o ~/bin/nvim"
     # curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o ~/bin/nvim
@@ -95,11 +116,16 @@ function __neovim_bin()
     # echo "# curl -L https://github.com/neovim/neovim/releases/download/v0.9.4/nvim.appimage -o ~/bin/nvim"
     # curl -L https://github.com/neovim/neovim/releases/download/v0.9.4/nvim.appimage -o ~/bin/nvim
     #### 0.9.5
-    echo "# curl -L https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage -o ~/bin/nvim"
-    curl -L https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage -o ~/bin/nvim
+    # echo "# curl -L https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage -o ~/bin/nvim"
+    # curl -L https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage -o ~/bin/nvim
+    #### 0.10.0
+    # echo "# curl -L https://github.com/neovim/neovim/releases/download/v0.10.0/nvim.appimage -o ~/bin/nvim"
+    # curl -L https://github.com/neovim/neovim/releases/download/v0.10.0/nvim.appimage -o ~/bin/nvim
+    # echo "# curl -L https://github.com/neovim/neovim-releases/releases/download/v0.10.0/nvim.appimage -o ~/bin/nvim"
+    # curl -L https://github.com/neovim/neovim-releases/releases/download/v0.10.0/nvim.appimage -o ~/bin/nvim
     ####
-    echo "# chmod +x ~/bin/nvim"
-    chmod +x ~/bin/nvim
+    # echo "# chmod +x ~/bin/nvim"
+    # chmod +x ~/bin/nvim
 }
 
 function __vim_vimplug()
@@ -116,6 +142,11 @@ function __neovim_vimplug()
 
 function __plugin_update()
 {
+    ## DEBUG
+    # vim +PlugInstall
+    # nvim +PlugInstall
+
+    ## NO DEBUG
     vim +PlugInstall +qall
     nvim +PlugInstall +qall
 }
