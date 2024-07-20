6188554bfb27c66f7ca3651cf3c73cf9b6b12d0b
Kresimir Marzic <kmarzic@gmail.com>
2024-07-20 09:57:23 +0200

    update

--- bin/xmenu.sh
+++ bin/xmenu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export PATH="${HOME}/bin:/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin"
+export PATH="${HOME}/bin:${HOME}/.local/bin:/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin"
 export _JAVA_AWT_WM_NONREPARENTING=1
 
-${HOME}/bin/xmenu -w <<EOF | sh &
+${HOME}/.local/bin/xmenu -w <<EOF | sh &
 Terminal (st)			st
 Terminal (xterm)		xterm
 Terminal (urxvt)		urxvt
