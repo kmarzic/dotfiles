50b9ba944ca32478b8436391366e7df380d5345f
Kresimir Marzic <kmarzic@gmail.com>
2024-09-08 07:18:54 +0200

    openwrt build: update

--- bin/openwrt.build.sh
+++ bin/openwrt.build.sh
@@ -139,16 +139,16 @@ function __immortalwrt_build()
             __printf "friendlyarm_nanopi-r6s" info
 
             #### (1)
-            # __printf "$ wget https://downloads.immortalwrt.org/snapshots/targets/rockchip/armv8/immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst" success
-            # wget https://downloads.immortalwrt.org/snapshots/targets/rockchip/armv8/immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst
+            __printf "$ wget https://downloads.immortalwrt.org/snapshots/targets/rockchip/armv8/immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst" success
+            wget https://downloads.immortalwrt.org/snapshots/targets/rockchip/armv8/immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst
 
             #### (2)
             # __printf "$ wget https://dl.wrt.moe/snapshots/targets/rockchip/armv8/immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst" success
             # wget https://dl.wrt.moe/snapshots/targets/rockchip/armv8/immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst
 
             #### (3)
-            __printf "$ wget https://immortalwrt.kyarucloud.moe/snapshots/targets/rockchip/armv8/immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst" success
-            wget https://immortalwrt.kyarucloud.moe/snapshots/targets/rockchip/armv8/immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst
+            # __printf "$ wget https://immortalwrt.kyarucloud.moe/snapshots/targets/rockchip/armv8/immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst" success
+            # wget https://immortalwrt.kyarucloud.moe/snapshots/targets/rockchip/armv8/immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst
 
             __printf "$ tar -I zstd -xf immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst" success
             tar -I zstd -xf immortalwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.zst
@@ -162,8 +162,8 @@ function __immortalwrt_build()
             __printf "$ sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=.*/CONFIG_TARGET_KERNEL_PARTSIZE=32/g" .config" success
             sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=.*/CONFIG_TARGET_KERNEL_PARTSIZE=32/g" .config
 
-            __printd "$ sed -i "s/downloads.immortalwrt.org/immortalwrt.kyarucloud.moe/g" repositories.conf" success
-            sed -i "s/downloads.immortalwrt.org/immortalwrt.kyarucloud.moe/g" repositories.conf
+            # __printd "$ sed -i "s/downloads.immortalwrt.org/immortalwrt.kyarucloud.moe/g" repositories.conf" success
+            # sed -i "s/downloads.immortalwrt.org/immortalwrt.kyarucloud.moe/g" repositories.conf
 
             __printf "$ make image PROFILE=friendlyarm_nanopi-r6s PACKAGES=\"\
 6in4 adblock banip block-mount bridge bzip2 comgt curl ddns-scripts-cloudflare ddns-scripts-freedns ddns-scripts-noip \
@@ -333,8 +333,8 @@ netperf openssl-util openvpn-openssl tcpdump vim wireguard-tools"
             [[ ${input1} == "n" ]] && __printf "exit..." && exit ${EXIT_ERROR}
             [[ ${input1} == "y" ]] && __printf "continue..."
 
-            __printf "$ scp -O bin/targets/ath79/mikrotik/openwrt-ath79-mikrotik-mikrotik_routerboard-962uigs-5hact2hnt-squashfs-sysupgrade.bin root@10.254.205.3:/tmp" success
-            scp -O bin/targets/ath79/mikrotik/openwrt-ath79-mikrotik-mikrotik_routerboard-962uigs-5hact2hnt-squashfs-sysupgrade.bin root@10.254.205.3:/tmp
+            __printf "$ scp -O bin/targets/ath79/mikrotik/openwrt-ath79-mikrotik-mikrotik_routerboard-962uigs-5hact2hnt-squashfs-sysupgrade.bin root@10.254.205.4:/tmp" success
+            scp -O bin/targets/ath79/mikrotik/openwrt-ath79-mikrotik-mikrotik_routerboard-962uigs-5hact2hnt-squashfs-sysupgrade.bin root@10.254.205.4:/tmp
             ;;
         #####################################################################
         "mikrotik_cap-ac")
